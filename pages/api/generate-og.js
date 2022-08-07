0ea3d8b63ec4532077961a62348facd0ab6a9280
Fardeen Ehsan <fardeen.es7@gmail.com>
2022-08-07 22:13:26 +0600

    initial commit

---
+++ pages/api/generate-og.js
@@ -0,0 +1,30 @@
+import chromium from 'chrome-aws-lambda'
+
+export default async function opengraph(req, res) {
+  // Parse the title
+  const { title } = req.query
+  const baseURL = req.headers.host
+
+  // Open the browser with the right window size
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: { width: 1200, height: 630 },
+    executablePath: await chromium.executablePath, // change for localhost
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
+
+  // Navigate a new browser page to the layout page
+  let page = await browser.newPage()
+  console.log(`http://localhost:3000/opengraph?title=${title}`)
+  await page.goto(`http://localhost:3000/opengraph?title=${title}`, { waitUntil: 'networkidle2' })
+
+  // Take a screenshot
+  const screenshotBuffer = await page.screenshot({ type: 'png' })
+  await browser.close()
+
+  // Tell the consuming service to cache the image being sent
+  res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`)
+  res.setHeader('Content-Type', 'image/png')
+  res.status(200).send(screenshotBuffer)
+}
---
+++ pages/opengraph.jsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+
+export default function Opengraph() {
+  const { query } = useRouter()
+  const { title } = query
+
+  return (
+    <div className='w-[1200px] h-[630px] bg-slate-700 flex items-center justify-center'>
+      <div class='flex flex-col h-[530px] w-[1100px] rounded-3xl bg-white items-center justify-between px-12 py-24'>
+        <h1 class='text-7xl grow font-semibold'>{title}</h1>
+        <span class='text-3xl'>Fardeen Ehsan</span>
+      </div>
+    </div>
+  )
+}
