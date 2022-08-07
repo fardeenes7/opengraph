f14e1c1727f5ccf3c15d1c5a24786b051dd76122
Fardeen Ehsan <fardeen.es7@gmail.com>
2022-08-07 22:18:40 +0600

    minor bug fix

--- pages/opengraph.jsx
+++ pages/opengraph.jsx
@@ -8,7 +8,7 @@ export default function Opengraph() {
     <div className='w-[1200px] h-[630px] bg-slate-700 flex items-center justify-center'>
       <div className='flex flex-col h-[530px] w-[1100px] rounded-3xl bg-white items-center justify-between px-12 py-24'>
         <h1 className='text-7xl grow font-semibold'>{title}</h1>
-        <span classname='text-3xl'>by Fardeen Ehsan</span>
+        <span className='text-3xl'>by Fardeen Ehsan</span>
       </div>
     </div>
   )
